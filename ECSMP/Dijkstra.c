b5cd5dc87da6a245c1d795759a1b0ee428df58df
Loic <loic.moncayo@hotmail.fr>
2014-11-23 22:50:40 +0100

    Début Djikstra
    Changement A* --> Djikstra, début de l'implémentation

---
+++ ECSMP/Dijkstra.c
@@ -0,0 +1,53 @@
+
+typedef struct Node{
+    int positionx;
+	int positiony;
+	int poids;
+	int visited;
+	Node antecedent;
+}Node;
+
+typedef struct chemin{
+
+    }
+
+
+void init_node(Node n,int px, int py,int i,int w,Node a){
+	n->positionx=px;
+	n->positiony=py;
+	n->poids=i;
+	n->visited=w;
+	n->antecedent=a;
+	}
+
+
+int main(int argc, char *argv[])
+
+Node* carte[10][10];
+
+for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            Node n;
+            init_node(n,i,j,0,1,NULL);
+            carte[i][j]=n;
+        }
+}
+
+Node noeudDepart;
+init_node(noeudDepart,0,0,0,1,NULL);
+
+
+int l,m;
+
+for (l=0;l<10;l++){
+    for(m=0;m<10;l++){
+        if(!carte[l][m].visited){
+            if()
+        }
+    }
+}
+
+
+
+
+
