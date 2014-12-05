3ee28bb9a09724a7d3a6b285d3afa200de2ca8c6
Jodu17 <phan.joseph7@gmail.com>
2014-12-05 22:45:47 +0100

    Agrandissement de la carte

--- ECSMP/carte.h
+++ ECSMP/carte.h
@@ -2,15 +2,14 @@
 #define CARTE_H_INCLUDED
 
 #define TAILLE_SPRITE 32
-#define NB_SPRITES_LARGEUR 10
-#define NB_SPRITES_HAUTEUR 10
+#define NB_SPRITES_LARGEUR 20
+#define NB_SPRITES_HAUTEUR 20
 #define LARGEUR_FENETRE TAILLE_SPRITE*NB_SPRITES_LARGEUR
 #define HAUTEUR_FENETRE TAILLE_SPRITE*NB_SPRITES_HAUTEUR
 
 enum {CHEMIN,HERBE};
 SDL_Surface *ecran,*sprites,*perso,*gland;
 SDL_Rect positionChemin,positionHerbe,PosFinal,positionPerso,posPerso,positionObjet;
-int carre[10][10];
 
 void formationCarte();
 void chargementCarte();
